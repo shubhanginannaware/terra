0074e7ea7a539eebc059106e5b15432c5a103b8f
Shubhangi Nannware <63737734+shubhanginannaware@users.noreply.github.com>
2024-10-22 16:04:53 +0530

    Delete devops directory

--- devops/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "demo-resources"
-    storage_account_name = "shubhangipipeline"
-    container_name = "prod-tfstate"
-    key = "prod.terraform.tfstate"
-  }
-}
